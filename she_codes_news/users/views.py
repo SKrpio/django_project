79444ce904835b04307a75146cbe2c5cd804ced6
SKrpio <134825964+SKrpio@users.noreply.github.com>
2023-07-15 12:33:50 +1000

    Session 4

---
+++ she_codes_news/users/views.py
@@ -0,0 +1,10 @@
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.views import generic
+from .models import CustomUser
+from .forms import CustomerUserCreationForm
+
+class CreateAccountView(CreateView):
+    form_class = CustomerUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'users/createAccount.html'
